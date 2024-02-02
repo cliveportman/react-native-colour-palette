fd084a6cf2e2d908ded122aa446fc3b8f102bdb5
Clive Portman <clive@cliveportman.co.uk>
2024-02-02 12:10:37 +0000

    refactor: adding ts

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import { View, Text, SafeAreaView, StyleSheet, SectionList } from 'react-native';
-import SafeViewAndroid from "./components/SafeViewAndroid";
-import ColouredBox from './components/ColouredBox';
+import SafeViewAndroid from "../components/SafeViewAndroid";
+import ColouredBox from '../components/ColouredBox';
 
 const tailwindColours = [
   { title: 'Slate', data: [{ name: 'Slate 950', hex: '#020617' }, { name: 'Slate 900', hex: '#0f172a' }, { name: 'Slate 800', hex: '#1e293b' }, { name: 'Slate 700', hex: '#334155' }, { name: 'Slate 600', hex: '#475569'}, { name: 'Slate 500', hex: '#64748b'}, { name: 'Slate 400', hex: '#94a3b8'}, { name: 'Slate 300', hex: '#cbd5e1'}, { name: 'Slate 200', hex: '#e2e8f0'}, { name: 'Slate 100', hex: '#f1f5f9'}, { name: 'Slate 50', hex: '#f8fafc'} ] },
@@ -33,12 +33,12 @@ const styles = StyleSheet.create({
   safe: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'start',
+    justifyContent: 'flex-start',
   },
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'start',
+    justifyContent: 'flex-start',
     margin: 10,
   },
   text: {
